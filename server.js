529d6403cfbfbcea084effab43cea293e64a1ee2
Smdgameking <130335449+Smdgameking@users.noreply.github.com>
2026-03-04 14:26:14 +0530

    Update API endpoint and add authorization headers

--- server.js
+++ server.js
@@ -65,13 +65,17 @@ app.post("/reco", async (req, res) => {
     try {
 
         const response = await fetch(
-            `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${process.env.API_KEY}`,
+            `https://openrouter.ai/api/v1/chat/completions`,
             {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
+                "Authorization":"Bearer ${process.env.API_KEY}",
+                "HTTP-Referer": "https://localhost",
+                "X-Title": "AI Career Recommendation"
                 },
                 body: JSON.stringify({
+                    model: "openai/gpt-4o-mini",
                     contents: [
                         {
                             parts: [
